7d17325c0cf0c23cf49419b660db909412c906d7
LingZhouChen <lingzhouc@gmail.com>
2023-10-02 17:11:18 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    // react component to render
+    <App />, 
+    
+    // where to render react component
+        // convention = div w/ ID of root
+    document.getElementById("root")
+);
